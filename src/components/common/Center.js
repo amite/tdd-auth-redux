0ad3f38bb56ea7315bdd1659dde03dd0f7e5afce
amite <amit.erandole@gmail.com>
2018-04-01 07:03:16 +0530

    adds resusable Center component using styled-components

---
+++ src/components/common/Center.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Center = styled.section`
+  background: papayawhip;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  height: 100vh;
+`;
+Center.displayName = "Center";
+
+export default Center;
